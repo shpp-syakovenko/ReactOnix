cee31cad182a31742456c3135cf2455b965ca419
syakovenko <serglife777@gmail.com>
2019-07-02 05:58:33 +0300

    last commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$(".toggle-mnu").click(function() {  // срабатывает бри клике на кнопку и закрытии
+	$(".toggle-mnu").click(function() { 	 // срабатывает бри клике на кнопку и закрытии(Для верхнего меню)
 		if($(this).hasClass('on')){
 			$(this).removeClass('on');
 			$('.menu').slideUp(500);
@@ -10,12 +10,21 @@ $(document).ready(function(){
 		}
 	});  
 
-	$('.slider').slick({			    // Стили для слайдера
+	$('.slider').slick({			   		 // Стили для слайдера
   		dots: true,
   		arrows: false,  		
 	});
 
-	$('.service-slide').click(function(){
+	$('.slider-say').slick({			   	 // Стили для слайдера, стрелки указываем свои
+  		dots: true,
+  		arrows: true,
+  		appendArrows:'.arrows',
+  		prevArrow:'.prev',
+ 		nextArrow:'.next',
+	});
+	
+
+	$('.service-slide').click(function(){    // Эфект для секции OUR SERVICES
 		let more = $(this).children('.service-slide-popup');
 		if(more.is(':visible')){
 			more.hide(200);
@@ -24,4 +33,22 @@ $(document).ready(function(){
 		}
 	});
 
+	$('.wrapper-team').click(function(){     // Эфект для секции Small Team
+		let more = $(this).children('.wrapper-team-description');
+		if(more.is(':visible')){
+			more.hide(200);
+		}else{
+			more.show(200);
+		}
+	});
+
+											// Плавный переход по якорным ссылка
+	$(document).ready(function(){
+    $(".menu").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+   	 	});
+	});
 });
