ac4af53ec5a9353048ccb0ac2050dce484a980ea
syakovenko <serglife777@gmail.com>
2019-10-01 10:10:03 +0300

    add pageUp

--- src/components/layout/Page/Page.js
+++ src/components/layout/Page/Page.js
@@ -4,16 +4,50 @@ import '../../../scss/layout/page.scss';
 import PageView from './PageView';
 
 class Page extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pageUpVisible: false,
+      scrolled: window.pageYOffset,
+      coords: document.documentElement.clientHeight
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.trackScroll);
+  }
+
+  trackScroll = () => {
+    const { scrolled, coords } = this.state;
+    this.setState({
+      pageUpVisible: scrolled > coords,
+      scrolled: window.pageYOffset
+    });
+  };
+
+  handleClickPageUp = () => {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(this.handleClickPageUp, 0);
+    }
+  };
+
   render() {
     const { children } = this.props;
+    const { pageUpVisible } = this.state;
     return (
-      <PageView page={children} />
+      <PageView
+        page={children}
+        handleClickPageUp={this.handleClickPageUp}
+        pageUpVisible={pageUpVisible}
+      />
     );
   }
 }
 
 Page.propTypes = {
-  children: PropTypes.element.isRequired
+  children: PropTypes.element.isRequired,
 };
 
 export default Page;
--- src/components/layout/Page/PageView.js
+++ src/components/layout/Page/PageView.js
@@ -2,20 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Button from '../../elements/Button/Button';
 
 
-const PageView = ({ page }) => {
+const PageView = ({ page, handleClickPageUp, pageUpVisible }) => {
   return (
     <div className="page">
       <Header />
       {page}
       <Footer />
+      {
+        pageUpVisible && (
+          <div className="pageUp">
+            <Button title="PageUp" onButtonClick={handleClickPageUp} />
+          </div>
+        )
+      }
     </div>
   );
 };
 
 PageView.propTypes = {
-  page: PropTypes.element.isRequired
+  page: PropTypes.element.isRequired,
+  handleClickPageUp: PropTypes.func,
+  pageUpVisible: PropTypes.bool
+};
+
+PageView.defaultProps = {
+  handleClickPageUp: undefined,
+  pageUpVisible: false
 };
 
 export default PageView;
